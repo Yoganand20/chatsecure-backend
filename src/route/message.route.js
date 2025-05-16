09c7ce6e68c0e1b377d23a40b463f99c2ce66d5d
Yoganand <59907866+Yoganand20@users.noreply.github.com>
2025-05-16 21:21:56 +0530

    Added Protect route

--- src/route/message.route.js
+++ src/route/message.route.js
@@ -1,11 +1,12 @@
 import express from "express";
 
+import { protectRoute } from '../middleware/auth.middleware.js';
 import { getRoomList, getMessagesByRoom, postMessages, createRoom} from "../controller/message/message.controller.js";
 
 const messageRouter=express.Router();
-messageRouter.get('/roomList',getRoomList);
-messageRouter.get('/:roomId',getMessagesByRoom);
-messageRouter.post('/send/:roomId/',postMessages);
-messageRouter.post('/createRoom/',createRoom);
+messageRouter.get('/roomList',protectRoute,getRoomList);
+messageRouter.get('/:roomId',protectRoute,getMessagesByRoom);
+messageRouter.post('/send/:roomId/',protectRoute,postMessages);
+messageRouter.post('/createRoom/',protectRoute,createRoom);
 
 export default messageRouter;
